6a9190ef7f96233fa7b20f557474f84635f2d8f8
luisviramontes <luis_alfonso133@hotmail.com>
2021-11-16 18:47:37 -0600

    FlatList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, Button,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, Button,ScrollView ,FlatList} from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
@@ -9,32 +9,52 @@ export default function App() {
   const [cambia, setCambia] = useState('')
   return (   
     <View style={styles.container}>
-       <ScrollView style={styles.scroll}>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <Text style={styles.texto}>Texto: {cambia}</Text>
-      <TextInput onChangeText={t => setCambia(t)} style={styles.input} defaultValue={text} placeholder="Ingrese el nombre del servicio" />
-      <Button onPress={() => {
-        setCambia(text)
-      }} title="Ingresar Text" />
-
-
+      <FlatList
+      data={[
+        {key:'1', name:'Leyi'},
+        {key:'2', name:'Diana'},
+        {key:'3', name:'Enrique'},
+        {key:'4', name:'Luis'},
+        {key:'5', name:'Ivan'},
+        {key:'6', name:'Alfonso'},
+        {key:'7', name:'Edson'},
+        {key:'8', name:'Aldo'},
+        {key:'9', name:'Victor'},
+        {key:'10', name:'Manuel'},
+        {key:'11', name:'Francisco'},
+        {key:'12', name:'cwervando'},
+        {key:'13', name:'Edgar'},
+        {key:'14', name:'Luis'},
+        {key:'15', name:'esaul'},
+        {key:'16', name:'Barajas'},
+        {key:'17', name:'Viramontes'},
+        {key:'18', name:'Zuñiga'},
+        {key:'19', name:'Del Rio'},
+        {key:'20', name:'Alejandro'},
+        {key:'21', name:'Leyi'},
+        {key:'22', name:'Diana'},
+        {key:'23', name:'Enrique'},
+        {key:'24', name:'Luis'},
+        {key:'25', name:'Ivan'},
+        {key:'26', name:'Alfonso'},
+        {key:'27', name:'Edson'},
+        {key:'28', name:'Aldo'},
+        {key:'29', name:'Victor'},
+        {key:'30', name:'Manuel'},
+        {key:'31', name:'Francisco'},
+        {key:'32', name:'cwervando'},
+        {key:'33', name:'Edgar'},
+        {key:'34', name:'Luis'},
+        {key:'35', name:'esaul'},
+        {key:'36', name:'Barajas'},
+        {key:'37', name:'Viramontes'},
+        {key:'38', name:'Zuñiga'},
+        {key:'39', name:'Del Rio'},
+        {key:'40', name:'Alejandro'},
+      ]}
+      renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
-      </ScrollView>
     </View >  
   );
 }
@@ -46,18 +66,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    height: 40,
-    backgroundColor: '#dfdf',
-    width: width,
-
-  },
-  texto: {
-    fontSize: 28,
-    color: 'blue',
-  },
-  scroll:{
-    width: Dimensions.get('window').width,
-
-  },
+  item:{
+    padding:20,
+    fontSize:22,
+    height:40,
+    borderBottomColor:'#ccc',
+    borderBottomWidth:1,
+  color:'red'
+  }
 });
