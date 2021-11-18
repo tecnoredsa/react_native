8225cd45598477f49873774537e070743fe5152e
luisviramontes <luis_alfonso133@hotmail.com>
2021-11-18 17:08:28 -0600

    ImageBack

--- App.js
+++ App.js
@@ -1,55 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image,ImageBackground } from 'react-native';
 
 export default function App() {
-
   return (
     <View style={styles.container}>
-      <View style={styles.column1}>
-        <Image
-          style={styles.head}
-          source={require('./assets/tca.png')}
-        />
+      <ImageBackground source={require('./assets/fondo.jpeg')} resizeMode="cover" style={styles.fondo}>
+        <View style={styles.column1}>
+          <Image
+            style={styles.head}
+            source={require('./assets/tca.png')}
+          />
 
-        <Image
-          style={styles.head}
-          source={require('./assets/tpu.png')}
-        />
-
-
-        <Image
-          style={styles.head}
-          source={{ uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/216.png' }}
-        />
-      </View>
-
-      <View style={styles.column2}>
+          <Image
+            style={styles.head}
+            source={require('./assets/tpu.png')}
+          />
 
 
+          <Image
+            style={styles.head}
+            source={{ uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/216.png' }}
+          />
+        </View>
 
-
-        <Image
-          style={styles.head}
-          source={require('./assets/tch.png')}
-        />
-
-        <Image
-          style={styles.head}
-          source={{ uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/225.png' }}
-        />
-
-        <Image
-          style={styles.head}
-          source={{ uri: 'https://a.espncdn.com/i/teamlogos/soccer/500/223.png' }}
-        />
-      </View>
+        <View style={styles.column2}>
 
 
 
 
+          <Image
+            style={styles.head}
+            source={require('./assets/tch.png')}
+          />
 
-      <StatusBar style="auto" />
+          <Image
+            style={styles.head}
+            source={{ uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/225.png' }}
+          />
+
+          <Image
+            style={styles.head}
+            source={{ uri: 'https://a.espncdn.com/i/teamlogos/soccer/500/223.png' }}
+          />
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View >
   );
 }
@@ -57,15 +53,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    flexDirection: 'row'
   },
   head: {
     height: 100,
     width: 100,
+    backgroundColor:'#000000cc',
   },
   column1: {
     alignItems: 'center',
@@ -75,5 +67,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fondo: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+    flexDirection: 'row',  },
 
 });
